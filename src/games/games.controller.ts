f86d08581654e84d561c7edf1441939ba36e06a5
Sebastian Herrmann <sebastian@herrherrmann.net>
2019-09-08 22:33:36 +0200

    Remove obsolete route

--- src/games/games.controller.ts
+++ src/games/games.controller.ts
@@ -10,11 +10,6 @@ export class GamesController {
     return await this.gamesService.getGames();
   }
 
-  @Get('/from-igdb')
-  async getGamesFromIGDB() {
-    return await this.gamesService.getGamesFromIGDB();
-  }
-
   @Get('/from-github')
   async getGamesFromGitHub() {
     return await this.gamesService.getGamesFromGitHub();
